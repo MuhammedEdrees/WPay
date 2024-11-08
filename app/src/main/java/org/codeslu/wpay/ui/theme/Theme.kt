2ea7d5b813fb541ae1ccec0377e5f0ae6aaf665c
edrees <muhammed.edrees101@gmail.com>
2024-11-08 22:41:55 +0200

    1-feat-implement-home-screen: Disabled dark mode

--- app/src/main/java/org/codeslu/wpay/ui/theme/Theme.kt
+++ app/src/main/java/org/codeslu/wpay/ui/theme/Theme.kt
@@ -35,7 +35,7 @@ private val LightColorScheme = lightColorScheme(
 
 @Composable
 fun WPayTheme(
-    darkTheme: Boolean = isSystemInDarkTheme(),
+//    darkTheme: Boolean = isSystemInDarkTheme(),
     // Dynamic color is available on Android 12+
     dynamicColor: Boolean = true,
     content: @Composable () -> Unit
@@ -43,10 +43,11 @@ fun WPayTheme(
     val colorScheme = when {
         dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
             val context = LocalContext.current
-            if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
+//            if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
+            dynamicLightColorScheme(context)
         }
 
-        darkTheme -> DarkColorScheme
+//        darkTheme -> DarkColorScheme
         else -> LightColorScheme
     }
 
