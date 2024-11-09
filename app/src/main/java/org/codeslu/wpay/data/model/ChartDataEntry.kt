83dd596e8488eb1a9f1dc3a543bb98f2965478ac
edrees <muhammed.edrees101@gmail.com>
2024-11-10 00:05:37 +0200

    5-feat-implement-statistics-screen: Added statistics screen data models

---
+++ app/src/main/java/org/codeslu/wpay/data/model/ChartDataEntry.kt
@@ -0,0 +1,9 @@
+package org.codeslu.wpay.data.model
+
+import kotlinx.datetime.LocalDate
+
+data class ChartDataEntry(
+    val title: String,
+    val income: Double,
+    val expenses: Double
+)
---
+++ app/src/main/java/org/codeslu/wpay/data/model/PieChartEntry.kt
@@ -0,0 +1,9 @@
+package org.codeslu.wpay.data.model
+
+import androidx.compose.ui.graphics.Color
+
+data class PieChartEntry(
+    val value: Double,
+    val label: String,
+    val color: Color
+)
